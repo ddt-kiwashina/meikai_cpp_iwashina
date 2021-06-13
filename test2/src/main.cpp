ee556871edd3b09d8376e8162110929da1df6085
岩品耕太 <>
2021-06-13 23:35:36 +0900

    eclipse 環境test

---
+++ test2/src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2021/06/13
+ *      Author: iwahinkota
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	cout << "Hello World " << endl;
+
+
+	return 0;
+}
+
